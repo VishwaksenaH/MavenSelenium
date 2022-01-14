b226616260578a1c6ea823ec1743490e7122ff3f
002V2D744 <002V2D744@LAPTOP-OILJ7GEB>
2022-01-15 00:37:10 +0530

    ...

--- MavenSeleniumProject/src/test/java/verifygoogletest/VerifygoogleTest.java
+++ MavenSeleniumProject/src/test/java/verifygoogletest/VerifygoogleTest.java
@@ -43,3 +43,63 @@ public class VerifygoogleTest {
 		}
 }
 
+/*import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class VerifygoogleTest {
+	//Annotation sequence
+    @BeforeSuite
+    public void setproperty()
+    {
+        System.out.println("set the system property");
+    }
+    
+    @BeforeClass
+    public void enterurl()
+    {
+        System.out.println("Open url");
+    }
+    
+    @BeforeTest
+    public void launchbrowser()
+    {
+        System.out.println("Open the browser");
+    }
+    @BeforeMethod
+    public void getURL()
+    {
+        System.out.println("display URL");
+    }
+    
+    @Test
+    public void checktitle()
+    {
+        System.out.println("check title of page");
+    }
+    
+    @AfterClass
+    public void closebrowser()
+    {
+        System.out.println("close browser");
+    }
+    
+    @AfterMethod
+    public void logout()
+    {
+        System.out.println("Logout");
+    }
+    
+    @AfterTest
+    public void checkresults()
+    {
+        System.out.println("result displayed");
+    }
+
+	
+	
+}*/
